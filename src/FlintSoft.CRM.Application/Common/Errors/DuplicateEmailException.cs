ff7b89d3ef68fb5e6521ee8fb79aab03252bf1c7
mprattinge <michael.prattinger@becom-group.com>
2024-09-12 13:45:26 +0200

    Better error message

--- src/FlintSoft.CRM.Application/Common/Errors/DuplicateEmailException.cs
+++ src/FlintSoft.CRM.Application/Common/Errors/DuplicateEmailException.cs
@@ -7,5 +7,5 @@ public class DuplicateEmailException : Exception, IServiceException
 {
     public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
 
-    public string ErrorMessage => "Email already exists";
+    public string ErrorMessage => "User already exists";
 }
